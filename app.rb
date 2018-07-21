8f03fa3567d1f316b0d0ebba2e56198dd7483148
derrick b <dbozich1@gmail.com>
2018-07-21 01:30:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 class Application < Sinatra::Base
   # Write your code here!
+  connect to views
+
+  get '/' do
+    erb :index
+  end
+
+  post '/' do
+
+  end
+
+  get '/greet' do
+    erb :greet
+  end
 
 end
